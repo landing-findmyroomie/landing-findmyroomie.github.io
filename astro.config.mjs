dc5ede275053b76a03c9696bfbe669bc00560e67
David Molina <dmolina23@gmail.com>
2024-04-01 20:56:18 +0200

    Configuring Google Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown"
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    })
+  ],
   site: 'https://landing-findmyroomie.github.io'
 });
